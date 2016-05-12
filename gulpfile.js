616b2126555a391dbcfebbaa4364afe7942645b7
jorgegvalencia <jorgegvalencia@gmail.com>
2016-05-12 23:19:13 +0200

    Cambio en el gulpfile para inyectar todos los .js del cliente.

--- gulpfile.js
+++ gulpfile.js
@@ -10,7 +10,7 @@ var paths = {
 	"index"      : "./app/frontend/index.html",
 	"dist"       : "./app/frontend/dist/",
 	"css"        : "./app/frontend/dist/*.css",
-	"javascript" : "./app/frontend/js/*.js",
+	"javascript" : "./app/frontend/js/**/*.js",
 	"less"       : "./app/frontend/less/style.less",
 }
 
